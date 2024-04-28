fb78c691ce39d80404eeac128cfeb0c7afb21f53
Luis Cepeda <luissebastian.cepedaflechas@gmail.com>
2024-04-28 12:04:26 -0500

    add prisma service

---
+++ src/prisma.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    private static instance: PrismaClient
+
+    async onModuleInit() {
+        console.log('Conexión creada')
+        await this.$connect()
+    }
+}
